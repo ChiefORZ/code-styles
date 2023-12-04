558f6177eff47ff5929b22d05814dd5ba0d90721
ChiefORZ <seb.schaffernak@gmail.com>
2023-12-04 17:35:23 +0700

    fix: disable @typescript-eslint/indent rule

--- packages/eslint-config-tslint-react/lib/tslint-config-react.js
+++ packages/eslint-config-tslint-react/lib/tslint-config-react.js
@@ -1,43 +1,44 @@
 module.exports = {
   extends: [
-    "airbnb-typescript",
-    "react-app",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'airbnb-typescript',
+    'react-app',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "@typescript-eslint/no-explicit-any": [
-          "error",
+        '@typescript-eslint/no-explicit-any': [
+          'error',
           {
             ignoreRestArgs: true,
           },
         ],
-        "@typescript-eslint/no-unused-vars": [
-          "warn",
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
           {
-            args: "none",
-            vars: "all",
+            args: 'none',
+            vars: 'all',
           },
         ],
-        "@typescript-eslint/semi": ["off"],
-        "import/no-extraneous-dependencies": [
-          "error",
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/semi': ['off'],
+        'import/no-extraneous-dependencies': [
+          'error',
           {
             devDependencies: [
-              "**/*.test.js",
-              "**/*.test.jsx",
-              "**/*.test.ts",
-              "**/*.test.tsx",
-              "src/tests/**/*",
+              '**/*.test.js',
+              '**/*.test.jsx',
+              '**/*.test.ts',
+              '**/*.test.tsx',
+              'src/tests/**/*',
             ],
           },
         ],
-        "import/prefer-default-export": "off",
-        "max-len": [
-          "warn",
+        'import/prefer-default-export': 'off',
+        'max-len': [
+          'warn',
           {
             code: 80,
             ignoreComments: true,
@@ -45,20 +46,20 @@ module.exports = {
             ignoreTemplateLiterals: true,
           },
         ],
-        "no-plusplus": [
-          "error",
+        'no-plusplus': [
+          'error',
           {
             allowForLoopAfterthoughts: true,
           },
         ],
-        "object-curly-spacing": ["warn", "always"],
-        "react/destructuring-assignment": "off",
-        "react/jsx-boolean-value": "off",
-        "react/jsx-key": "error",
-        "react/jsx-one-expression-per-line": "off",
-        "react/jsx-props-no-spreading": "off",
-        "react/jsx-sort-props": [
-          "error",
+        'object-curly-spacing': ['warn', 'always'],
+        'react/destructuring-assignment': 'off',
+        'react/jsx-boolean-value': 'off',
+        'react/jsx-key': 'error',
+        'react/jsx-one-expression-per-line': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/jsx-sort-props': [
+          'error',
           {
             callbacksLast: false,
             ignoreCase: true,
@@ -67,35 +68,31 @@ module.exports = {
             shorthandLast: false,
           },
         ],
-        "react/jsx-wrap-multilines": "off",
-        "react/no-unescaped-entities": "off",
-        "react/prop-types": "off",
-        "sort-keys-fix/sort-keys-fix": "warn",
-        "validate-jsx-nesting/no-invalid-jsx-nesting": "error",
+        'react/jsx-wrap-multilines': 'off',
+        'react/no-unescaped-entities': 'off',
+        'react/prop-types': 'off',
+        'sort-keys-fix/sort-keys-fix': 'warn',
+        'validate-jsx-nesting/no-invalid-jsx-nesting': 'error',
       },
     },
     {
-      files: ["*.json"],
-      plugins: ["json-files"],
+      files: ['*.json'],
+      plugins: ['json-files'],
       rules: {
-        "json-files/require-engines": "warn",
-        "json-files/require-license": "warn",
-        "json-files/require-unique-dependency-names": "error",
-        "json-files/sort-package-json": "warn",
+        'json-files/require-engines': 'warn',
+        'json-files/require-license': 'warn',
+        'json-files/require-unique-dependency-names': 'error',
+        'json-files/sort-package-json': 'warn',
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
-  plugins: [
-    "sort-keys-fix",
-    "validate-jsx-nesting",
-    "@typescript-eslint",
-  ],
+  plugins: ['sort-keys-fix', 'validate-jsx-nesting', '@typescript-eslint'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
       },
